3d1014b19c670d2d1473be76f1b84accd4c7a16f
Christopher Jael Montero <cristhofermontero1@gmail.com>
2020-09-18 13:19:51 -0400

    4 Commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
-'use strict'
+document.querySelector('.btn-hidden-social-network')
+    .addEventListener('click', () => {
+        document.querySelector('.btn-hidden-social-network')
+            .style.display = 'none';
 
-document.querySelector('#btn-hidden-social-network').addEventListener('click', () => {
-    document.querySelector('.social_network').style.display = 'block';
-    document.querySelector('#btn-hidden-social-network').style.display = 'none';
-})
+        document.querySelector('.social_network')
+            .style.display = 'inline';
+    })
+
+document.querySelector('.btn-back')
+    .addEventListener('click',() => {
+        document.querySelector('.social_network')
+            .style.display = 'none';
+
+        document.querySelector('.btn-hidden-social-network')
+            .style.display = 'inline';
+    })
